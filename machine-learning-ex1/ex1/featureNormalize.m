8155a9a2baa329d9b9b4c08bf2c11aec9881d197
Nishant Suneja <nishant.suneja@maxta.com>
2017-09-27 01:42:28 -0700

    [ex1] Implementing feature normalization

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -26,6 +26,14 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+num_features = size(X,2)
+for i=1:num_features
+    mu(1, i) = mean(X(:, i))
+    sigma(1, i) = std(X(:, i))
+    X_norm(:, i) = X_norm(:, i) - mu(1, i)
+    X_norm(:, i) = X_norm(:,i) / sigma(1, i)
+end
+
 
 
 
