b4e67bdc67b32d05c870a2c61d5850ff30731199
Nishant Suneja <nishant.suneja@maxta.com>
2017-09-27 01:40:12 -0700

    [ex1] Implementing gradient descent algorithm for linear regression

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -6,7 +6,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+num_features = length(theta) % Num of features should be 2.
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,18 +16,17 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    gamma = zeros(num_features, 1)
+    for j=1:num_features
+       for i=1:m
+          gamma(j,1) = gamma(j,1) + (((theta' * X(i,:)') - y(i,1)) .* X(i,j))
+       end
+    end
+    theta = theta - alpha * (1/m) * gamma
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-
-end
 
 end
