998419e19645d4ad1e0b2ede913e3ef48236ea01
Nishant Suneja <nishant.suneja@maxta.com>
2017-09-27 22:48:48 -0700

    [ex2] Logistic regression with regularization

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,35 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+n_features = size(theta)(1)
 
+cost1 = 0
+for i=1:m
+    h_i = sigmoid(theta'*X(i,:)')
+    cost1 = cost1 + (-y(i)*log(h_i) - (1-y(i))*(log(1-h_i)))
+end
+cost1 = (1.0/m) * cost1
 
+cost2 = 0
+for i=2:n_features % We dont regularize theta(0)
+    cost2 = cost2 + (theta(i,1) ^ 2)
+end
+cost2 = (lambda/(2.0*m)) * cost2
 
+J = cost1 + cost2
+
+for i=1:n_features
+    gradient = 0
+    for j=1:m
+       h_j = sigmoid(theta'*X(j,:)')
+       gradient = gradient + (h_j - y(j,1))*X(j,i)
+    end
+    if (i == 1)
+       grad(i,1) = (1.0/m) * gradient
+    else
+       grad(i,1) = (1.0/m) * gradient + (lambda*1.0/m)*theta(i,1)
+    endif
+end
 
 
 % =============================================================
